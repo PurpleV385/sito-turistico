3d3ae3499ad215e97028aa13ae654878045a760f
PurpleV385 <purple.v385@gmail.com>
2024-12-24 17:44:30 +0100

    Add files via upload

--- galleria.js
+++ galleria.js
@@ -6,18 +6,55 @@ document.addEventListener('DOMContentLoaded', (event) => {
       const response = await fetch('http://localhost:3000/get-filenames');
       const data = await response.json();
       const fileList = data.files;
-      const imageContainer = document.querySelector('.image-container');
-      // console.log(data.files.length);
-      for(let i = 0; i < data.files.length; i++){
-        // console.log(data.files[i]);
+      const soggiorno = document.querySelector('.soggiorno');
+      const cucina = document.querySelector('.cucina');
+      const camere = document.querySelector('.camere');
+      const bagno = document.querySelector('.bagno');
+      const esterno = document.querySelector('.esterno');
+      console.log(data.files.length);
+      for (let i = 0; i < data.files.length; i++) {
         const image = document.createElement('img');
-        image.src = `../Immagini sito/${data.files[i]}`;
+        const imageContainer = document.createElement('div');
         imageContainer.appendChild(image);
+        imageContainer.classList.add('image-container');
+        image.src = `../Immagini sito/${data.files[i]}`;
+        if (data.files[i][0] == "a") {
+          soggiorno.querySelector('.immagini').appendChild(imageContainer);
+          console.log("cont è malato");
+        }
+        if (data.files[i][0] == "b") {
+          cucina.querySelector('.immagini').appendChild(imageContainer);
+        }
+        if (data.files[i][0] == "c") {
+          camere.querySelector('.immagini').appendChild(imageContainer);
+        }
+        if (data.files[i][0] == "d") {
+          bagno.querySelector('.immagini').appendChild(imageContainer);
+        }
+        if (data.files[i][0] == "e") {
+          esterno.querySelector('.immagini').appendChild(imageContainer);
+        }
+        if (data.files[i] == "logo.png") {
+          continue;
+        }
+
+        // console.log(data.files[0]);
+
+        // data.files.forEach((index, file) => {
+        //   // console.log('file');
+        //   // console.log(file);
+
+        // });
       }
-    } catch (error) {
+    }
+    catch (error) {
       console.error('Error fetching filenames:', error);
     }
   }
 
+
   fetchFilenames();
+  // document.querySelector('#prova').addEventListener('click', () => { //esegue il codice ogni volta che si clicca il bottone
+
+  // });
 });
